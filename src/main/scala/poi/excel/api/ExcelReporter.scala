4702295739494232ef84fa9f7f6cfc9d61ddeaa9
Sergey Pichkurov <sergey.pichkurov@vistajet.com>
2015-01-13 14:58:06 +0200

    initial reporter api with samples

---
+++ src/main/scala/poi/excel/api/ExcelReporter.scala
@@ -0,0 +1,53 @@
+package poi.excel.api
+
+/**
+ * Created by serp on 1/13/2015.
+ */
+object ExcelReporter {
+
+
+  /**
+   * Internal data type
+   */
+  object DataType extends Enumeration {
+    val Number, String, Date = Value
+  }
+  
+  /**
+   * 
+   * @param field   data field name (map key)
+   * @param title   title 
+   * @param dataType data type               
+   * @param format  format string to visualize
+   */
+  case class ColInfo(field: String, dataType: DataType.Value, title:String, format: Option[String] = None )
+
+  // meta column list
+  type ColInfoList = List[ColInfo]
+
+
+  // list of strings
+  type RowGroupList = List[String]
+
+  // 2 dims report data
+  type ReportData = List[List[String]]
+
+  // report info
+  case class ReportInfo( colList: ColInfoList, rowGroup:RowGroupList = Nil )
+
+  type ReportFile = Any // TDB provide the actual data type
+
+  trait Reporter {
+    def run( reportInfo: ReportInfo, reportData: ReportData) : ReportFile
+  }
+
+  def makeReporter() : Reporter = {
+    return new Reporter {
+      override def run(reportInfo: ReportInfo, reportData: ReportData): ReportFile = {
+        // TDB write the actual code here
+      }
+    }
+  }
+
+
+}
