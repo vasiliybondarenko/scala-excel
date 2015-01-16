8811a5e57701534048484d6d2a89e247cf2cb081
shredinger <vasiliy.bondarenko@gmail.com>
2015-01-16 14:22:06 +0200

    Code cleanup

--- src/main/scala/poi/excel/CellType.scala
+++ src/main/scala/poi/excel/CellType.scala
@@ -1,7 +1,6 @@
 package poi.excel
 
 import java.text.SimpleDateFormat
-import java.util.Date
 
 import org.apache.poi.xssf.usermodel.XSSFCell
 import poi.excel.api.ExcelReporter
@@ -13,34 +12,6 @@ import poi.excel.api.ExcelReporter
  * Time: 11:03 PM 
  * Project: scala-excel
  */
-object CellDataTypes{
-  val CURRENCY = "^\\d*\\.\\d*$"
-  val DATE = "\\d{4}"
-}
-
-
-abstract class CellData[+T]() {
-  def formatString:String
-  def getValue:T
-}
-
-case class Currency(value:String) extends CellData[Double]{
-  override def formatString: String = "$#,##0.00"
-  override def getValue: Double = value.toDouble
-}
-
-case class  DateCellType(value:String) extends CellData[Date]{
-  val formatter = new SimpleDateFormat(formatString)
-  override def getValue: Date = formatter.parse(value)
-  override def formatString: String = "dd/mm/yyyy"
-}
-
-case class Text(value:String) extends CellData[String]{
-  override def formatString: String = ""
-  override def getValue: String = value
-}
-
-//for new API
 
 abstract class CellDataConverter{
   def getDataFormat:String
